1367c4a25e681a69a64d2b8c38c58f0c90085b3a
diegogd04 <diego2612004@gmail.com>
2023-10-24 16:19:49 +0200

    Librerías y permisos de Internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,33 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    val lifecycle_version = "2.6.2"
+
+    // by ViewModels en Activity
+    implementation("androidx.activity:activity-ktx:1.7.2")
+
+    // Ciclo Vida: ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+
+    // Ciclo Vida: LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Json
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Skeleton
+    implementation("com.faltenreich:skeletonlayout:5.0.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Conversor
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
