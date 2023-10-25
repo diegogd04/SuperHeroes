720749834b25f60f20eb71bd046e2dbfdfe780ff
diegogd04 <diego2612004@gmail.com>
2023-10-25 20:01:58 +0200

    Se ponen todos los datos.

--- app/src/main/java/com/dgd/superheroes/data/remote/api/Mappers.kt
+++ app/src/main/java/com/dgd/superheroes/data/remote/api/Mappers.kt
@@ -3,4 +3,13 @@ package com.dgd.superheroes.data.remote.api
 import com.dgd.superheroes.domain.Heroe
 
 fun HeroeApiModel.toModel(): Heroe =
-    Heroe(this.id, this.name, this.images)
+    Heroe(this.id, this.name, this.slug, this.powerstats.toModel(), this.apperance.toModel(), this.images.toModel())
+
+fun HeroeApiModel.Powerstats.toModel(): Heroe.Powerstats =
+    Heroe.Powerstats(this.intelligence, this.strength, this.speed, this.durability, this.power, this.combat)
+
+fun HeroeApiModel.Appearance.toModel(): Heroe.Appearance =
+    Heroe.Appearance(this.gender, this.race, this.height, this.weight, this.eyeColor, this.hairColor)
+
+fun HeroeApiModel.Images.toModel(): Heroe.Images =
+    Heroe.Images(this.xs, this.sm, this.md, this.lg)
--- app/src/main/java/com/dgd/superheroes/domain/Heroe.kt
+++ app/src/main/java/com/dgd/superheroes/domain/Heroe.kt
@@ -3,5 +3,33 @@ package com.dgd.superheroes.domain
 data class Heroe(
     val id: Int,
     val name: String,
-    val images: String
-)
+    val slug: String? = null,
+    val powerstats: Powerstats,
+    val appearance: Appearance,
+    val images: Images
+){
+    data class Powerstats(
+        val intelligence: Int,
+        val strength: Int,
+        val speed: Int,
+        val durability: Int,
+        val power: Int,
+        val combat: Int
+    )
+
+    data class Appearance(
+        val gender: String,
+        val race: String?,
+        val height: List<String>,
+        val weight: List<String>,
+        val eyeColor: String,
+        val hairColor: String
+    )
+
+    data class Images(
+        val xs: String,
+        val sm: String,
+        val md: String,
+        val lg: String
+    )
+}
